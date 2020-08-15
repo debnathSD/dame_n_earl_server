38cc79a86f65fae6ef6322db10fd3a94f47a03a1
Sourav Debnath <sourav2012d@gmail.com>
2020-08-16 02:35:14 +0530

    Connects MongoDB Hosted In AWS

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Configuration
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB via Mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`MongoDB Connected!`))
+  .catch((err) => console.log(`Uh-Oh! ${err}`));
 
 app.get("/", (req, res) => res.send(`Hey Base Route!!!`));
 
