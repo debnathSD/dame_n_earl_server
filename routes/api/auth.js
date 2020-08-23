490700dab9910b1ee997f3accc7960420e095a7a
Sourav Debnath <sourav2012d@gmail.com>
2020-08-23 13:11:22 +0530

    Adds Password Validation For User Login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -48,4 +48,33 @@ router.post("/register", (req, res) => {
   });
 });
 
+/**
+ * @route   POST /api/v1/auth/login
+ * @desc    LOGIN User and Return a JSON WEB TOKEN (JWT)
+ * @access  Public
+ */
+router.post("/login", (req, res) => {
+  const email = req.body.email,
+    password = req.body.password;
+
+  // Find a User By Email
+  User.findOne({ email }).then((user) => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found!" });
+    }
+
+    // Check Password
+    // Compare plain text password with
+    // The hashed password stored in DB
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        res.json({ message: "LOGIN SUCCESS!" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect!" });
+      }
+    });
+  });
+});
+
 module.exports = router;
