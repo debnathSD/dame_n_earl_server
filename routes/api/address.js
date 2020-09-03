8dcca660dc0ad78a32344ad49c22b836fba2cd9a
sweta <swt.shw94@gmail.com>
2020-09-03 17:14:06 +0530

    Updates User Address

--- routes/api/address.js
+++ routes/api/address.js
@@ -26,5 +26,28 @@ const User = require("../../models/User");
       }
     });
   });
+
+/**
+ * @route   GET /api/v1/auth/addAddress
+ * @desc    Adds new address to address list
+ * @access  Public
+ */
+
+ router.post("/delAddress", (req,res) => {
+   const id = req.body.id;
+   User.updateOne({ email: req.body.email }, {"$pull": { c_addresses: {"_id": id} }} ).then((user) => {
+    if (user) {
+          res.status(200).json({
+            addressDeleted: "True"
+          });
+    } else {
+      return res.status(400).json({
+        email: "User doesn't exist",
+      });
+    }
+  });
+
+ });
+
   
 module.exports = router;
