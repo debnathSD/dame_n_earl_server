fa54e4684652035b8fd555a690cec618ca25c2c7
sweta <swt.shw94@gmail.com>
2020-09-07 01:53:48 +0530

    Minor: address --> addresses

--- routes/api/user.js
+++ routes/api/user.js
@@ -14,17 +14,18 @@ const User = require("../../models/User");
 router.post("/updateUser", (req, res) => {
   const _gender = req.body.gender;
   const _contactno = req.body.contactno;
-  const _address = req.body.address;
+  const _addresses = req.body.addresses;
 
-  User.findOneAndUpdate({ email: req.body.email }, {gender: _gender,
-                                                    contactno: _contactno,
-                                                    $set: {addresses: _address},
-                                                    
-                                                  }).then((user) => {
+  User.findOneAndUpdate({ email: req.body.email }, {
+    gender: _gender,
+    contactno: _contactno,
+    $set: { addresses: _addresses },
+
+  }).then((user) => {
     if (user) {
-          res.status(200).json({
-            isUpdated: "True"
-          });
+      res.status(200).json({
+        isUpdated: "True"
+      });
     } else {
       return res.status(400).json({
         email: "User doesn't exist",
@@ -41,19 +42,19 @@ router.post("/updateUser", (req, res) => {
  * @access  Public
  */
 
- router.get("/getUserDetails", (req, res) => {
+router.get("/getUserDetails", (req, res) => {
   const email = req.body.email;
 
-// Find a User By Email
-User.findOne({ email }).then((user) => {
-  // Check for user
-  if (!user) {
-    return res.status(404).json({ email: "User not found!" });
-  }else{
-    return res.send(user);
-  }
+  // Find a User By Email
+  User.findOne({ email }).then((user) => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found!" });
+    } else {
+      return res.send(user);
+    }
 
- });
- });
+  });
+});
 
 module.exports = router;
