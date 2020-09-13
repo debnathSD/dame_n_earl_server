14cd413ef1e91a8b35214b593ffd8372048ffb7a
Sourav Debnath <sourav2012d@gmail.com>
2020-09-13 21:18:11 +0530

    Refines /getCartDetails Route

--- routes/api/product.js
+++ routes/api/product.js
@@ -6,92 +6,24 @@ const router = express.Router();
 const User = require("../../models/Product");
 
 /**
- * @route   GET /api/v1/products/getProductsDetails/indian_wear
+ * @route   GET /api/v1/products/getProductsDetails/:attureType
+ * @param {attireType} The Product
  * @desc    Get Products Detailsz
  * @access  Public
  */
-
-router.get("/getProductsDetails/Indian_Wear", (req, res) => {
-    
-  //const _category = req.body.category;
+router.get("/getProductsDetails/:attireType", (req, res) => {
+  const _category = req.params.attireType;
   // Get all the products
-  Product.find({ category: "Indian_Wear" }).then((category) => {
+  Product.find({ category: _category }).then((category) => {
     // Check if category exists
     if (!category) {
-      return res.status(404).json({ category: "Category not found!" });
-    }else{
-      return res.json(category);
+      return res.status(404).json({ attireDetails: "Category not found!" });
+    } else {
+      return res.status(200).json({ attireDetails: category });
     }
-  
-   });
-   });
+  });
+});
 
-/**
- * @route   GET /api/v1/products/getProductsDetails/Earrings
- * @desc    Get all Earings Details
- * @access  Public
- */
-
-router.get("/getProductsDetails/Earrings", (req, res) => {
-    
-  //const _category = req.body.category;
-  // Get all the products
-  Product.find({ category: "Earrings" }).then((category) => {
-    // Check if category exists
-    if (!category) {
-      return res.status(404).json({ category: "Category not found!" });
-    }else{
-      return res.json(category);
-    }
-  
-   });
-   });
-
-   /**
- * @route   GET /api/v1/products/getProductsDetails/Nosepins
- * @desc    Get all Nosepins Details
- * @access  Public
- */
-
-router.get("/getProductsDetails/Nosepins", (req, res) => {
-    
-  //const _category = req.body.category;
-  // Get all the products
-  Product.find({ category: "Nosepins" }).then((category) => {
-    // Check if category exists
-    if (!category) {
-      return res.status(404).json({ category: "Category not found!" });
-    }else{
-      return res.json(category);
-    }
-  
-   });
-   });
-
-   /**
- * @route   GET /api/v1/products/getProductsDetails/headAccessories
- * @desc    Get all headAccessories Details
- * @access  Public
- */
-
-router.get("/getProductsDetails/headAccessories", (req, res) => {
-    
-  //const _category = req.body.category;
-  // Get all the products
-  Product.find({ category: "headAccessories" }).then((category) => {
-    // Check if category exists
-    if (!category) {
-      return res.status(404).json({ category: "Category not found!" });
-    }else{
-      return res.json(category);
-    }
-  
-   });
-   });
-
-
-
-  
 /**
  * @route   POST /api/v1/products/addProducts
  * @desc    Get Products Details
@@ -99,21 +31,19 @@ router.get("/getProductsDetails/headAccessories", (req, res) => {
  */
 
 router.post("/addProducts", (req, res) => {
-    const _category = req.body.category;
-    const _items = req.body.items;
-    Product.create({ category: _category, items: _items}, (err) => {
-      if(err){
-        console.log(err);
-        res.json({
-          error:
-            "Something bad happened while storing products in Product Schema!",
-        });
-      }
-      else{
-        res.json({isProductsAdded: "true"});
-      }
-    });
-     });
-    
+  const _category = req.body.category;
+  const _items = req.body.items;
+  Product.create({ category: _category, items: _items }, (err) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        error:
+          "Something bad happened while storing products in Product Schema!",
+      });
+    } else {
+      res.json({ isProductsAdded: "true" });
+    }
+  });
+});
 
-  module.exports = router;
+module.exports = router;
